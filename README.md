8d928377fb043bb40c659fd8d8d30af49ee2c4b4
David Rios <david.rios.gomes@gmail.com>
2013-02-26 13:48:05 -0300

    finished dot text block highlighting

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Jade.tmbundle
 
-**This was made specifically for Sublime Text 2. It might also work in Textmate but it's untested**
+**This was made specifically for Sublime Text 2. It might also work in Textmate but it's not tested**
 
 A **TextMate Bundle** for the **Jade** templating language. Implemented in
 JSON-tmLanguage, a compiled tmLanguage version is included. All language
-features that I know of and some indent increase/decrease patterns are
-implemented.
+features that I know of (including some undocumented ones and quircks of the
+Jade parser) and some indent increase/decrease patterns are implemented.
 
-A test.jade file is included with all features I know, it is a valid file, so
-you can compile it with jade to check its output.
+A test.jade file is included for testing, it is a valid file, so you can
+compile it with jade to check its output.
+
+A test_errors.jade file is also included for testing some syntax/semantic errors
+that are highlighted.
 
 ## Installation
 
--- Syntaxes/dot_block_tag.py
+++ Syntaxes/dot_block_tag.py
@@ -1,5 +1,4 @@
 import re
-from timeit import timeit
 
 
 def collapse_re(exp):
@@ -9,51 +8,60 @@ def collapse_re(exp):
     exp = exp.replace(' ', '')
     return exp
 
-tag_id = r'(#[\w-]+)'
+tag_id = r'(:REP:#[\w-]+)'
 
-tag_class = r'(\.[\w-]+)'
+tag_class = r'(:REP:\.[\w-]+)'
 
-tag_name = r'''(
-  ([#!]\{[^}]*\}) # interpolated name
+tag_name = r'''(:REP:
+  (?:[#!]\{[^}]*\}) # interpolated name
   | # or
-  (\w(([\w:-]+[\w-])|([\w-]*))) # regular name
+  (?:\w(?:(?:[\w:-]+[\w-])|(?:[\w-]*))) # regular name
 )'''
 
 # unless they are inside strings, we dont support parens inside
 # attrs, since it's impossible to detect if they are balanced
-tag_attrs = r'(\(([^()\'\"]*((\'([^\']|((?<!\\)\\\'))*\')|(\"([^\"]|((?<!\\)\\\"))*\")))*[^()]*\))*'
+tag_attrs = r'(?:\((?:[^()\'\"]*(?:(?:\'(?:[^\']|(?:(?<!\\)\\\'))*\')|(?:\"(?:[^\"]|(?:(?<!\\)\\\"))*\")))*[^()]*\))*'
+
+pure_tag = r'''
+(?: # a pure tag (name/id/classes)
+  (?: # starting with id or class
+    %(tag_id)s
+    | # or
+    %(tag_class)s
+  )
+  | # or
+  %(tag_name)s # a tag name
+)
+''' % {'tag_id': tag_id, 'tag_class': tag_class, 'tag_name': tag_name}
 
 tag = r'''
-( # a jade tag starts with
-  ( # a pure tag (name/id/classes)
-    ( # starting with id or class
-      %(tag_id)s
-      | # or
-      %(tag_class)s
-    )
-    | # or
-    %(tag_name)s # a tag name
-  )
-  ( # followed by an optional number of
+(?: # a jade tag starts with
+  %(pure_tag)s # a pure tag (name/id/classes)
+  (?: # followed by an optional number of
     %(tag_id)s
     | # or
     %(tag_class)s
     | # or
     %(tag_attrs)s
   )*
-)''' % {'tag_id': tag_id, 'tag_class': tag_class, 'tag_name': tag_name, 'tag_attrs': tag_attrs}
+)''' % {'pure_tag': pure_tag, 'tag_id': tag_id, 'tag_class': tag_class, 'tag_attrs': tag_attrs}
 
 dot_block_tag = r'''
 (?=[\w.#].*?\.$) # first, let's see if it meets the most basic requirements
-(?=( # a dot text block tag
-  ((:\s+)?%(tag)s)+ # is a tag followed by any number of subtags
+(?=(?: # a dot text block tag
+  (?:(?::\s+)?%(tag)s)+ # is a tag followed by any number of subtags
 )\.$) # and ending the line with a '.'
-''' % {'tag': tag}
+''' % {'tag': tag.replace(':REP:', '?:')}
+
+dot_block_tag_capturing = r'''
+%(dot_block_tag)s
+%(pure_tag)s
+''' % {'dot_block_tag': dot_block_tag, 'pure_tag': pure_tag.replace(':REP:', '')}
 
 dot_block_tag = collapse_re(dot_block_tag)
 
 if __name__ == '__main__':
-    r = re.compile(dot_block_tag)
+    r = re.compile(collapse_re(dot_block_tag_capturing))
     for i in [
       ': asdfsdf.',
       '.',
@@ -72,13 +80,16 @@ if __name__ == '__main__':
       'atag.cls.cls#id.',
       '.cls: atag.cls: #idtag.',
       'atag(attr, attr2=abc, attr3="te(sdfsd)st").',
-      'atag(attr, attr2=abc, attr3="te(s\\"dfsd)st").',
+      '#tagid(attr, attr2=abc, attr3="te(s\\"dfsd)st").',
       "atag(attr, attr2=abc, attr3='te(s\\'dfsd)st', asdfasdf=')', asdf=\"asdf))\").",
       'atag(attr, attr2=2)(more="attrs"): withsub: .cls(abc=1).',
       'div.class.another: p.red(style="").asdfsd(lol=123)#ohno.',
       '#{null+123{}.',
       'abc(123)asd.'  # oddly this should match, despite an unexpected output
     ]:
-        print '"%s": %s' % (i, r.match(i))
+        print '"%s": %s' % (i, r.match(i) and r.match(i).groups())
 
+    print "normal --------------"
     print dot_block_tag.replace('\\', '\\\\').replace('"', '\\"')
+    print "capturing -----------"
+    print collapse_re(dot_block_tag_capturing)
