31d521f60547bc259741829f4f1e317c78acf136
David Rios <david.rios.gomes@gmail.com>
2013-03-04 19:49:42 -0300

    removed unneeded rule
    now that the regexp is capturing, there's no need for separate rules for indented and not indented dot text blocks.

--- Syntaxes/dot_block_tag.py
+++ Syntaxes/dot_block_tag.py
@@ -63,33 +63,33 @@ dot_block_tag = collapse_re(dot_block_tag)
 if __name__ == '__main__':
     r = re.compile(collapse_re(dot_block_tag_capturing))
     for i in [
-      ': asdfsdf.',
-      '.',
-      'atag',
-      'atag.cls.cls#id',
-      '.cls: atag.cls: #idtag',
-      '.cls lorem ipsun.',
-      '.cls= lorem ipsun.',
-      'atag(attr, attr2=(1+1), attr3=3).',
-      'atag(attr, attr2=(1+1), attr3=3)(more="attrs"): withsub: .cls(abc=1).',
-      'atag(attr, att="("): asdf r2= abc, attr3="test"().',
-      'atag(attr, att="("): asdf (r2= abc, attr3="test").',
-      'atag(attr, attr2=abc, attr3="te(s"dfsd)st").',
-      '#{null+123}}.',
-      'atag:  :  asdfdsf.',
-      'atag.cls.cls#id.',
-      '.cls: atag.cls: #idtag.',
-      'atag(attr, attr2=abc, attr3="te(sdfsd)st").',
-      '#tagid(attr, attr2=abc, attr3="te(s\\"dfsd)st").',
-      "atag(attr, attr2=abc, attr3='te(s\\'dfsd)st', asdfasdf=')', asdf=\"asdf))\").",
-      'atag(attr, attr2=2)(more="attrs"): withsub: .cls(abc=1).',
-      'div.class.another: p.red(style="").asdfsd(lol=123)#ohno.',
-      '#{null+123{}.',
-      'abc(123)asd.'  # oddly this should match, despite an unexpected output
+        ': asdfsdf.',
+        '.',
+        'atag',
+        'atag.cls.cls#id',
+        '.cls: atag.cls: #idtag',
+        '.cls lorem ipsun.',
+        '.cls= lorem ipsun.',
+        'atag(attr, attr2=(1+1), attr3=3).',
+        'atag(attr, attr2=(1+1), attr3=3)(more="attrs"): withsub: .cls(abc=1).',
+        'atag(attr, att="("): asdf r2= abc, attr3="test"().',
+        'atag(attr, att="("): asdf (r2= abc, attr3="test").',
+        'atag(attr, attr2=abc, attr3="te(s"dfsd)st").',
+        '#{null+123}}.',
+        'atag:  :  asdfdsf.',
+        'atag.cls.cls#id.',
+        '.cls: atag.cls: #idtag.',
+        'atag(attr, attr2=abc, attr3="te(sdfsd)st").',
+        '#tagid(attr, attr2=abc, attr3="te(s\\"dfsd)st").',
+        "atag(attr, attr2=abc, attr3='te(s\\'dfsd)st', asdfasdf=')', asdf=\"asdf))\").",
+        'atag(attr, attr2=2)(more="attrs"): withsub: .cls(abc=1).',
+        'div.class.another: p.red(style="").asdfsd(lol=123)#ohno.',
+        '#{null+123{}.',
+        'abc(123)asd.'  # oddly this should match, despite an unexpected output
     ]:
         print '"%s": %s' % (i, r.match(i) and r.match(i).groups())
 
-    print "normal --------------"
-    print dot_block_tag.replace('\\', '\\\\').replace('"', '\\"')
-    print "capturing -----------"
+    # print "normal --------------"
+    # print dot_block_tag.replace('\\', '\\\\').replace('"', '\\"')
+    # print "capturing -----------"
     print collapse_re(dot_block_tag_capturing).replace('\\', '\\\\').replace('"', '\\"')
